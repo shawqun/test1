81a3421ec602e23c1c467a481e52bdf6c346d5af
shawqun <xxxxiaoqun1995@163.com>
2020-06-07 18:22:43 +0800

    hh

---
+++ demo/src/main/java/com/amarsoft/first/Test.java
@@ -0,0 +1,43 @@
+package com.amarsoft.first;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/a")
+public class Test {
+
+    @RequestMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile file, HttpServletRequest request) throws IOException {
+//        String contextPath = request.getServletContext().getContextPath();
+
+        String originalFilename = file.getOriginalFilename();
+
+        String path = "demo/src/main/resources/static/files/";
+        File fileDir = new File(path);
+        //递归生成文件夹
+        if (!fileDir.exists()){
+            fileDir.mkdir();
+        }
+
+        File newFile = new File(fileDir.getAbsolutePath() + File.separator + originalFilename);
+
+        file.transferTo(newFile);
+        //111111111
+
+        return "success";
+    }
+
+    @RequestMapping("/hello")
+    public String hello(){
+
+        return "hello";
+    }
+}
---
+++ demo/src/test/java/com/amarsoft/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amarsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
